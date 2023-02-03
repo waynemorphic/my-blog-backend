c3c2d4ba711d896e292dedaaecefa5d2cf6b60d4
waynemorphic <wayneklyde@gmail.com>
2023-02-03 19:11:02 +0300

    Convert entity to DTO with a getter method

---
+++ src/main/java/com/blog/blogpost/writer/DTOMappingService.java
@@ -0,0 +1,36 @@
+package com.blog.blogpost.writer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class DTOMappingService {
+    @Autowired
+    private WriterRepository writerRepository;
+
+    // Method to return a list of WriterDTO
+    public List<WriterDTO> getAllBloggers(){
+        return ((List<Writer>) writerRepository
+            .findAll())
+            .stream()
+            .map(this::DTOConverter)
+                .collect(Collectors.toList());
+    }
+
+    // DTO converter that returns WriterDTO
+    private WriterDTO DTOConverter(Writer writer){
+        
+        // Instance of WriterDTO class
+        WriterDTO writerDTO = new WriterDTO();
+
+        // set name and email in DTO from the writer instance
+        writerDTO.setName(writer.getName());
+        writerDTO.setEmail(writer.getEmail());
+
+        return writerDTO;
+
+    }
+}
